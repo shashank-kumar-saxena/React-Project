45242efa511c1f38d1171b6e068c268de012a79c
Shashank <shashanksaxena226@gmail.com>
2022-08-30 12:18:35 +0530

    rf

---
+++ Different React Files/components/books.js
@@ -0,0 +1,25 @@
+var books = [
+    {
+        name: 'one',
+        id: 1,
+        title: "Bitcoin",
+        Author: 'Antony lewis',
+        img: 'https://m.media-amazon.com/images/I/819bH1CNx5L._AC_UY327_FMwebp_QL65_.jpg',
+        txxt: ' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis placeat quas vel iure corrupti dicta tenetur facere sequi veritatis eos!'
+    },
+    {
+        name: 'two',
+        id: 2,
+        title: "Bitcoin",
+        Author: 'Ajanta & Vivek',
+        img: 'https://images-eu.ssl-images-amazon.com/images/I/51CksOYaN3L._AC_SX184_.jpg'
+    },
+    {
+        name: 'three',
+        id: 3,
+        title: 'Teen Machine',
+        Author: 'Ishita Agarwal',
+        img: 'https://images-eu.ssl-images-amazon.com/images/I/41B2C2Frv5L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg'
+    }]
+
+export default books;
